50ba6ecbc193955e69037a03fb25b16faa39a51f
Harrymissi <415110593@qq.com>
2019-04-09 17:36:07 -0400

    fix only dispaly on chrome

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -4,10 +4,22 @@ import rootReducer from '../store/reducers/index';
 
 const initialState = {};
 const middleware = [thunk];
+const whichCompose = () => {
+    if (window.navigator.userAgent.includes("Chrome")) {
+        compose(
+            applyMiddleware(...middleware),
+            // Implements the Chrome redux tools extension
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        );
+    } else {
+        compose(applyMiddleware(...middleware));
+    }
+};
 const store = createStore(
     rootReducer,
     initialState,
-    compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+    whichCompose(),
     );
 
 export default store;
